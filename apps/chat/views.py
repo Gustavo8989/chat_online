b568ef71952d3a2b738d7f8c4de95c695f572f5d
Gustavo <gustavohenriquealves418@gmail.com>
2025-03-27 20:51:09 -0300

    Atualização do front-end

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import Message 
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html',{})
-
+    message = Message.objects.all() 
+    return render(request,'index.html',{'massage':message})
 
